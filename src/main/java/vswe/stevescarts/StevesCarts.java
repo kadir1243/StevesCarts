35b3d2447a863312b2f54c2851df9418312fc926
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-08 07:49:32 +0530

    Metadata

---
+++ src/main/java/vswe/stevescarts/StevesCarts.java
@@ -0,0 +1,10 @@
+package vswe.stevescarts;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StevesCarts implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/vswe/stevescarts/client/StevesCartsClient.java
@@ -0,0 +1,10 @@
+package vswe.stevescarts.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StevesCartsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
