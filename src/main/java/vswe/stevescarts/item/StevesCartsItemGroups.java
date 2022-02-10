76bcb63bd5abd4dac449367a005ea39652218148
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-10 19:50:54 +0530

    Itemgroup icons fix

--- src/main/java/vswe/stevescarts/item/StevesCartsItemGroups.java
+++ src/main/java/vswe/stevescarts/item/StevesCartsItemGroups.java
@@ -4,8 +4,9 @@ import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import vswe.stevescarts.StevesCarts;
+import vswe.stevescarts.block.StevesCartsBlocks;
 
 public class StevesCartsItemGroups {
-	public static final ItemGroup COMPONENTS = FabricItemGroupBuilder.create(StevesCarts.id("components_group")).icon(() -> new ItemStack(StevesCartsItems.IRON_WHEELS)).build();
-	public static final ItemGroup BLOCKS = FabricItemGroupBuilder.create(StevesCarts.id("blocks_group")).icon(() -> new ItemStack(StevesCartsItems.IRON_WHEELS)).build();
+	public static final ItemGroup COMPONENTS = FabricItemGroupBuilder.create(StevesCarts.id("components_group")).icon(() -> new ItemStack(StevesCartsItems.REINFORCED_WHEELS)).build();
+	public static final ItemGroup BLOCKS = FabricItemGroupBuilder.create(StevesCarts.id("blocks_group")).icon(() -> new ItemStack(StevesCartsBlocks.CART_ASSEMBLER)).build();
 }
