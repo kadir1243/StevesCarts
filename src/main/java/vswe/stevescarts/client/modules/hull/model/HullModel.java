88bfe125ae3a5613034919e12cc894b25e0d976b
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-10 17:20:13 +0530

    Add components

--- src/main/java/vswe/stevescarts/client/modules/hull/model/HullModel.java
+++ src/main/java/vswe/stevescarts/client/modules/hull/model/HullModel.java
@@ -1,12 +1,18 @@
 package vswe.stevescarts.client.modules.hull.model;
 
 import net.minecraft.client.model.ModelPart;
+import net.minecraft.util.Identifier;
 import vswe.stevescarts.client.modules.model.ModuleModel;
 
 public class HullModel extends ModuleModel {
 	public HullModel() {
 		super();
-		ModelPart bottom = new ModelPart();
-		this.addModel();
+//		ModelPart bottom = new ModelPart();
+//		this.addModel();
+	}
+
+	@Override
+	public Identifier getTexture() {
+		return null; // TODO
 	}
 }
--- src/main/java/vswe/stevescarts/item/StevesCartsItemGroups.java
+++ src/main/java/vswe/stevescarts/item/StevesCartsItemGroups.java
@@ -2,8 +2,9 @@ package vswe.stevescarts.item;
 
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import vswe.stevescarts.StevesCarts;
 
 public class StevesCartsItemGroups {
-	public static final ItemGroup COMPONENTS = FabricItemGroupBuilder.create(StevesCarts.id("components_group")).icon(StevesCartsItems.IRON_WHEELS::getDefaultStack).build();
+	public static final ItemGroup COMPONENTS = FabricItemGroupBuilder.create(StevesCarts.id("components_group")).icon(() -> new ItemStack(StevesCartsItems.IRON_WHEELS)).build();
 }
