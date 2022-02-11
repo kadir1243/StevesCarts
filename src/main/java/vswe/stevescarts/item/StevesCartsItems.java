d9114c18f7320f0a879063ec983263c9c3bd2759
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-11 13:40:24 +0530

    modular cart

--- src/main/java/vswe/stevescarts/item/StevesCartsItems.java
+++ src/main/java/vswe/stevescarts/item/StevesCartsItems.java
@@ -9,7 +9,7 @@ import vswe.stevescarts.modules.MinecartModuleType;
 public class StevesCartsItems {
 //	public static final ModularMinecartItem WOODEN_HULL = register("wooden_hull", new ModularMinecartItem(new Item.Settings(), MinecartModuleType.WOODEN_HULL));
 
-	public static final Item MODULAR_CART = register("modular_cart", new Item(new Item.Settings().maxCount(1)));
+	public static final ModularMinecartItem MODULAR_CART = register("modular_cart", new ModularMinecartItem(new Item.Settings().maxCount(1)));
 
 	// Components
 	public static final CartComponentItem WOODEN_WHEELS = registerSimpleComponent("wooden_wheels");
