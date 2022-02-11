382da05d9e907422390f467927c7ed58773a09b3
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-11 21:39:21 +0530

    Upgrade shapes

--- src/main/java/vswe/stevescarts/block/UpgradeBlock.java
+++ src/main/java/vswe/stevescarts/block/UpgradeBlock.java
@@ -2,17 +2,23 @@ package vswe.stevescarts.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
 public class UpgradeBlock extends Block {
 	public static final BooleanProperty CONNECTED = BooleanProperty.of("connected");
+	private static final VoxelShape DISCONNECTED_SHAPE;
+	private static final VoxelShape[] DIRECTIONAL_SHAPES;
 
 	public UpgradeBlock(Settings settings) {
 		super(settings.nonOpaque());
@@ -49,5 +55,26 @@ public class UpgradeBlock extends Block {
 			return this.getDefaultState().with(CONNECTED, true).with(Properties.FACING, ctx.getPlayerLookDirection().getOpposite());
 		}
 		return super.getPlacementState(ctx);
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		if (state.get(CONNECTED)) {
+			return DIRECTIONAL_SHAPES[state.get(Properties.FACING).getId()];
+		}
+		return DISCONNECTED_SHAPE;
+	}
+	
+	static {
+		double margin = 0.1875;
+		double thickness = 0.125;
+		DIRECTIONAL_SHAPES = new VoxelShape[6];
+		DISCONNECTED_SHAPE = VoxelShapes.cuboid(margin, thickness, margin, 1 - margin, 1 - thickness, 1 - margin);
+		DIRECTIONAL_SHAPES[Direction.DOWN.getId()] = VoxelShapes.cuboid(margin, 0, margin, 1 - margin, thickness, 1 - margin);
+		DIRECTIONAL_SHAPES[Direction.UP.getId()] = VoxelShapes.cuboid(margin, 1 - thickness, margin, 1 - margin, 1, 1 - margin);
+		DIRECTIONAL_SHAPES[Direction.WEST.getId()] = VoxelShapes.cuboid(0, margin, margin, thickness, 1 - margin, 1 - margin);
+		DIRECTIONAL_SHAPES[Direction.EAST.getId()] = VoxelShapes.cuboid(1 - thickness, margin, margin, 1, 1 - margin, 1 - margin);
+		DIRECTIONAL_SHAPES[Direction.NORTH.getId()] = VoxelShapes.cuboid(margin, margin, 0, 1 - margin, 1 - margin, thickness);
+		DIRECTIONAL_SHAPES[Direction.SOUTH.getId()] = VoxelShapes.cuboid(margin, margin, 1 - thickness, 1 - margin, 1 - margin, 1);
 	}
 }
