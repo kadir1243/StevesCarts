7551cce56c2f802f7337b17c26d3ab26f5ea8863
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-18 10:08:19 +0530

    Packet, dehardcode

--- src/main/java/vswe/stevescarts/screen/CartAssemblerHandler.java
+++ src/main/java/vswe/stevescarts/screen/CartAssemblerHandler.java
@@ -6,14 +6,19 @@ import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
 import io.github.cottonmc.cotton.gui.widget.WPlayerInvPanel;
 import io.github.cottonmc.cotton.gui.widget.WWidget;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.world.World;
+import vswe.stevescarts.StevesCarts;
 import vswe.stevescarts.block.StevesCartsBlocks;
 import vswe.stevescarts.block.entity.CartAssemblerBlockEntity;
 import vswe.stevescarts.item.ModularMinecartItem;
@@ -34,6 +39,7 @@ import java.util.List;
 
 public class CartAssemblerHandler extends SyncedGuiDescription {
 	private final ScreenHandlerContext context;
+	private final WItemSlot outputSlot;
 
 	public CartAssemblerHandler(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
 		super(StevesCartsScreenHandlers.CART_ASSEMBLER, syncId, playerInventory, getBlockInventory(context, CartAssemblerBlockEntity.SIZE), getBlockPropertyDelegate(context));
@@ -49,46 +55,53 @@ public class CartAssemblerHandler extends SyncedGuiDescription {
 		rootPanel.add(assembleButton, 330, 142, 79, 9);
 		WCart cart = new WCart(() -> null, 187, 120);
 		this.addCentered(cart, 4);
-		WItemSlot hullSlot = WItemSlot.outputOf(this.blockInventory, 0);
+		WItemSlot hullSlot = WItemSlot.outputOf(this.blockInventory, CartAssemblerBlockEntity.HULL_SLOT);
 		hullSlot.setFilter(MinecartModuleType::isHull);
 		hullSlot.addChangeListener(((slot, inventory, index, stack) -> {
 			assembleButton.setEnabled(MinecartModuleType.isHull(stack));
 		}));
 		rootPanel.add(hullSlot, 12, 18);
-		WModuleSlot engineSlots = new WModuleSlot(this.blockInventory, 1, 5, 1, ModuleCategory.ENGINE);
+		WModuleSlot engineSlots = new WModuleSlot(this.blockInventory, CartAssemblerBlockEntity.ENGINE_SLOT_START, 5, 1, ModuleCategory.ENGINE);
 		engineSlots.setFilter(MinecartModuleType::isEngine);
 		rootPanel.add(engineSlots, 7, 59);
 		// TODO: set filters
-		WModuleSlot toolSlot = new WModuleSlot(this.blockInventory, 6, 1, 1, ModuleCategory.TOOL);
+		WModuleSlot toolSlot = new WModuleSlot(this.blockInventory, CartAssemblerBlockEntity.TOOL_SLOT, 1, 1, ModuleCategory.TOOL);
 		rootPanel.add(toolSlot, 7, 89);
-		WModuleSlot attachmentSlots = new WModuleSlot(this.blockInventory, 7, 6, 1, ModuleCategory.ATTACHMENT);
+		WModuleSlot attachmentSlots = new WModuleSlot(this.blockInventory, CartAssemblerBlockEntity.ATTACHMENT_SLOT_START, 6, 1, ModuleCategory.ATTACHMENT);
 		rootPanel.add(attachmentSlots, 7, 119);
-		WModuleSlot storageSlots = new WModuleSlot(this.blockInventory, 13, 4, 1, ModuleCategory.STORAGE);
+		WModuleSlot storageSlots = new WModuleSlot(this.blockInventory, CartAssemblerBlockEntity.STORAGE_SLOT_START, 4, 1, ModuleCategory.STORAGE);
 		rootPanel.add(storageSlots, 7, 149);
-		WModuleSlot addonsSlots = new WModuleSlot(this.blockInventory, 17, 6, 2, ModuleCategory.ADDON);
+		WModuleSlot addonsSlots = new WModuleSlot(this.blockInventory, CartAssemblerBlockEntity.ADDON_SLOT_START, 6, 2, ModuleCategory.ADDON);
 		rootPanel.add(addonsSlots, 7, 179);
-		WItemSlot outputSlot = WItemSlot.outputOf(this.blockInventory, 29);
-		outputSlot.setFilter((stack) -> stack.isOf(StevesCartsItems.MODULAR_CART));
-		rootPanel.add(outputSlot, 330, 182);
-		WItemSlot fuelSlot = WItemSlot.outputOf(this.blockInventory, 30);
+		this.outputSlot = WItemSlot.outputOf(this.blockInventory, CartAssemblerBlockEntity.OUTPUT_SLOT);
+		this.outputSlot.setFilter((stack) -> stack.isOf(StevesCartsItems.MODULAR_CART));
+		rootPanel.add(this.outputSlot, 330, 182);
+		WItemSlot fuelSlot = WItemSlot.outputOf(this.blockInventory, CartAssemblerBlockEntity.FUEL_SLOT);
 		rootPanel.add(fuelSlot, 376, 182);
-		assembleButton.setOnClick(() -> {
-			if (!this.blockInventory.getStack(29).isEmpty()) {
-				return;
-			}
-			List<MinecartModule> modules = new ArrayList<>();
-			for (int i = 0; i < this.blockInventory.size(); i++) {
-				ItemStack stack = this.blockInventory.getStack(i);
-				if (stack.isEmpty()) {
-					continue;
-				}
-				modules.add(((ModuleItem) stack.getItem()).getType().createModule());
-				stack.decrement(1);
-			}
-			ItemStack stack = ModularMinecartItem.create(modules);
-			this.blockInventory.setStack(29, stack);
-		});
+		assembleButton.setOnClick(() -> ClientPlayNetworking.send(StevesCartsScreenHandlers.PACKET_ASSEMBLE_CLICK, PacketByteBufs.empty()));
 		rootPanel.validate(this);
+	}
+
+	public static void handleAssembleClick(ServerPlayerEntity player) {
+		ScreenHandler screenHandler = player.currentScreenHandler;
+		if (!(screenHandler instanceof CartAssemblerHandler handler)) {
+			StevesCarts.LOGGER.error("Received assemble click packet from non-cart assembler screen handler");
+			return;
+		}
+		if (!handler.blockInventory.getStack(CartAssemblerBlockEntity.OUTPUT_SLOT).isEmpty()) {
+			return;
+		}
+		List<MinecartModule> modules = new ArrayList<>();
+		for (int i = 0; i < handler.blockInventory.size(); i++) {
+			ItemStack stack = handler.blockInventory.getStack(i);
+			if (stack.isEmpty()) {
+				continue;
+			}
+			modules.add(((ModuleItem) stack.getItem()).getType().createModule());
+			stack.decrement(1);
+		}
+		ItemStack stack = ModularMinecartItem.create(modules);
+		handler.blockInventory.setStack(CartAssemblerBlockEntity.OUTPUT_SLOT, stack);
 	}
 
 	@Override
--- src/main/java/vswe/stevescarts/screen/StevesCartsScreenHandlers.java
+++ src/main/java/vswe/stevescarts/screen/StevesCartsScreenHandlers.java
@@ -1,13 +1,17 @@
 package vswe.stevescarts.screen;
 
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.util.Identifier;
 import vswe.stevescarts.StevesCarts;
 
 public class StevesCartsScreenHandlers {
 	public static final ScreenHandlerType<CartAssemblerHandler> CART_ASSEMBLER = ScreenHandlerRegistry.registerSimple(StevesCarts.id("cart_assembler"), (syncId, playerInventory) -> new CartAssemblerHandler(syncId, playerInventory, ScreenHandlerContext.EMPTY));
+	public static final Identifier PACKET_ASSEMBLE_CLICK = StevesCarts.id("assemble_click");
 
 	public static void init() {
+		ServerPlayNetworking.registerGlobalReceiver(PACKET_ASSEMBLE_CLICK, (server, player, handler, buf, responseSender) -> server.execute(() -> CartAssemblerHandler.handleAssembleClick(player)));
 	}
 }
