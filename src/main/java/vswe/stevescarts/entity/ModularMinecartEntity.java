ccc1c6ba3e18455970e07f6bbf2ad0ce75af64d6
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-19 09:04:01 +0530

    Use allmatch instead of negated anymatch

--- src/main/java/vswe/stevescarts/entity/ModularMinecartEntity.java
+++ src/main/java/vswe/stevescarts/entity/ModularMinecartEntity.java
@@ -136,7 +136,7 @@ public class ModularMinecartEntity extends AbstractMinecartEntity {
 
 	@Environment(EnvType.CLIENT)
 	public boolean shouldRenderTop() {
-		return this.getModuleList().stream().anyMatch(module -> !module.getType().shouldRenderTop());
+		return this.getModuleList().stream().allMatch(module -> module.getType().shouldRenderTop());
 	}
 
 	private int nextId() {
