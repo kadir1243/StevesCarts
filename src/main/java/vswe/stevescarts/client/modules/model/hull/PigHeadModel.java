1b5429b2233ada314696664c56d6c96bbf4b6e26
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-19 09:54:51 +0530

    Fix head

--- src/main/java/vswe/stevescarts/client/modules/model/hull/PigHeadModel.java
+++ src/main/java/vswe/stevescarts/client/modules/model/hull/PigHeadModel.java
@@ -16,7 +16,7 @@ public class PigHeadModel extends ModuleModel {
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		modelPartData.addChild("tail1", ModelPartBuilder.create().uv(0, 0).cuboid("head", -8.0f, -6.0f, -1.0f, 16, 12, 2).uv(16, 16).cuboid("snout", -2.0f, 0.0f, -9.0f, 4, 3, 1), ModelTransform.of(-9.0f, -5.0f, 0.0f, 0.0f, 1.5707964f, 0.0f));
+		modelPartData.addChild("full_head", ModelPartBuilder.create().uv(0, 0).cuboid("head", -4.0f, -4.0f, -8.0f, 8, 8, 8).uv(16, 16).cuboid("snout", -2.0f, 0.0f, -9.0f, 4, 3, 1), ModelTransform.of(-9.0f, -5.0f, 0.0f, 0.0f, 1.5707964f, 0.0f));
 		return TexturedModelData.of(modelData, 64, 32);
 	}
 }
