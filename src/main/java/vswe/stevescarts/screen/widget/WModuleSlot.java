933da73977adc15fc0b0448a917c6cba193a1f41
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-22 16:32:28 +0530

    Possibly figure out the right way

--- src/main/java/vswe/stevescarts/screen/widget/WModuleSlot.java
+++ src/main/java/vswe/stevescarts/screen/widget/WModuleSlot.java
@@ -38,9 +38,10 @@ public class WModuleSlot extends WItemSlot {
 		matrices.translate(0F, (top - 10)/0.8F - (top - 10) + 1, 0F);
 		ScreenDrawing.drawString(matrices, slot.category.getTranslation().asOrderedText(), HorizontalAlignment.LEFT, left + 5, top - 8, 0, slot.category.getTextColor());
 		matrices.pop();
-		int index = slot.startIndex;
+
 		for (int y = 0; y < slot.slotsHigh; ++y) {
 			for (int x = 0; x < slot.slotsWide; ++x) {
+				int index = slot.startIndex + x + y * slot.slotsWide;
 				ScreenDrawing.texturedRect(matrices, left + x * 18, top + y * 18, 18, 18, OPEN_TEXTURE, 0xFFFFFFFF);
 				ScreenDrawing.drawBeveledPanel(matrices, left + x * 18, top + y * 18, 18, 18, 0xB8000000, 0x4C000000, 0xB8FFFFFF);
 				ModuleSlot mSlot = slot.peers.get(index);
@@ -55,7 +56,6 @@ public class WModuleSlot extends WItemSlot {
 				int height = (int) ((1 - progress) * 8);
 				ScreenDrawing.texturedRect(matrices, xPos, yPos, 16, height, image, u1, v1, u2, v2, 0xFFFFFFFF);
 				ScreenDrawing.texturedRect(matrices, xPos, yPos + 8 + (8 - height), 16, height, image, u1, v2, u2, v2 + ((1 - progress) * 8) / 32.0F, 0xFFFFFFFF);
-				index++;
 			}
 		}
 	};
