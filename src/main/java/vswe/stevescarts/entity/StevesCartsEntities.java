1ece161ddc17717b52842ecaa517f37da5b82570
SD <harrypotter.starwars.shrish@gmail.com>
2022-08-02 16:36:50 +0530

    Assembler ui, 2

--- src/main/java/vswe/stevescarts/entity/StevesCartsEntities.java
+++ src/main/java/vswe/stevescarts/entity/StevesCartsEntities.java
@@ -1,8 +1,11 @@
 package vswe.stevescarts.entity;
 
+import vswe.stevescarts.StevesCarts;
+
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.registry.Registry;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 
@@ -10,5 +13,6 @@ public class StevesCartsEntities {
 	public static final EntityType<CartEntity> CART = FabricEntityTypeBuilder.create(SpawnGroup.MISC, CartEntity::new).dimensions(EntityDimensions.fixed(1, 1)).build();
 
 	public static void init() {
+		Registry.register(Registry.ENTITY_TYPE, StevesCarts.id("cart"), CART);
 	}
 }
